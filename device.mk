85d13e3705a6397fb37526439c41eba6d50fe8e8
DqrKnz <DavidYonatan9@hotmail.com>
2022-04-10 20:47:22 +0000

    alioth: move nfc configs to configs/

--- device.mk
+++ device.mk
@@ -542,7 +542,7 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # OMX
 PRODUCT_PACKAGES += \
